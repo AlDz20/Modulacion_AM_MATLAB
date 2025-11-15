56f9271f460b9e27c141fda3f1ddf645b5afef75
AlDz20 <alyson.diaz@hotmail.com>
2025-11-15 17:56:18 -0600

    Delete Modulacion directory

--- Modulacion/Modulacion/Códigos/a.m
+++
@@ -1,8 +0,0 @@
-distorsion = modulada_con_ruido.^2;
-
-figure;
-plot(t, distorsion);
-title('Señal con distorsión');
-xlabel('Tiempo (s)');
-ylabel('Amplitud');
-grid on;
--- Modulacion/Modulacion/Códigos/t.m
+++
@@ -1,9 +0,0 @@
-ruido = 0.5 * randn(size(modulada));
-modulada_con_ruido = modulada + ruido;
-
-figure;
-plot(t, modulada_con_ruido);
-title('Señal modulada con ruido');
-xlabel('Tiempo (s)');
-ylabel('Amplitud');
-grid on;
